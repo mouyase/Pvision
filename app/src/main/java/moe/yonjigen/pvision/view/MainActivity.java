98bf3ff14b92632e6ab419cb42497d4fcd8b673a
某亚瑟 <mouyase@qq.com>
2019-07-01 02:20:57 +0800

    WiP:Update

--- app/src/main/java/moe/yonjigen/pvision/view/MainActivity.java
+++ app/src/main/java/moe/yonjigen/pvision/view/MainActivity.java
@@ -3,29 +3,10 @@ package moe.yonjigen.pvision.view;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import moe.yonjigen.pvision.R;
-import moe.yonjigen.pvision.databinding.ActivityMainBinding;
-import moe.yonjigen.pvision.model.MainModel;
-import moe.yonjigen.pvision.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        activityMainBinding.setMain(new MainModel());
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-        layoutManager.setOrientation(RecyclerView.VERTICAL);
-        recyclerView.setLayoutManager(layoutManager);
-        MainViewModel mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        mViewModel.getLiveCardModelList().observe(this, (cardModelList) -> {
-            recyclerView.setAdapter(new MainCardAdapter(this, cardModelList));
-        });
     }
 }
--- app/src/main/java/moe/yonjigen/pvision/view/MainCardAdapter.java
+++ app/src/main/java/moe/yonjigen/pvision/view/MainCardAdapter.java
@@ -1,87 +1,74 @@
-package moe.yonjigen.pvision.view;
-
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import androidx.annotation.NonNull;
-import androidx.databinding.BindingAdapter;
-import androidx.databinding.DataBindingUtil;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.model.GlideUrl;
-import com.bumptech.glide.load.model.LazyHeaders;
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
-import com.bumptech.glide.request.target.Target;
-
-import java.util.List;
-
-import moe.yonjigen.pvision.PvisionData;
-import moe.yonjigen.pvision.R;
-import moe.yonjigen.pvision.databinding.CardMainBinding;
-import moe.yonjigen.pvision.model.MainCardModel;
-import moe.yonjigen.pvision.utils.ImageUtil;
-
-public class MainCardAdapter extends RecyclerView.Adapter<MainCardAdapter.ViewHolder> {
-    private List<MainCardModel> cardModelList;
-    private Context context;
-    private static LazyHeaders lazyHeaders = new LazyHeaders.Builder()
-            .setHeader("Referer", PvisionData.REFERER)
-            .setHeader("User-Agent", PvisionData.USER_AGENT)
-            .build();
-
-    /**
-     * @param context       上下文
-     * @param cardModelList 数据源
-     */
-    MainCardAdapter(Context context, List<MainCardModel> cardModelList) {
-        this.cardModelList = cardModelList;
-        this.context = context;
-    }
-
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        CardMainBinding binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.card_main, parent, false);
-        return new ViewHolder(binding);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.binding.setCard(cardModelList.get(position));
-        holder.binding.executePendingBindings();
-    }
-
-
-    @Override
-    public int getItemCount() {
-        return cardModelList.size();
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder {
-
-        CardMainBinding binding;
-
-        ViewHolder(CardMainBinding binding) {
-            super(binding.getRoot());
-            this.binding = binding;
-        }
-    }
-
-    @BindingAdapter("imageUrl")
-    public static void loadImage(ImageView imageView, String url) {
-        GlideUrl glideUrl = new GlideUrl(url, lazyHeaders);
-        Glide.with(imageView.getContext())
-                .load(glideUrl)
-                .transition(DrawableTransitionOptions.withCrossFade(100))
-                .override(Target.SIZE_ORIGINAL)
-                .override(ImageUtil.getImageWidth(imageView), ImageUtil.getImageHeight(imageView))
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(imageView);
-
-    }
-}
+//package moe.yonjigen.pvision.view;
+//
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.databinding.BindingAdapter;
+//import androidx.databinding.DataBindingUtil;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import com.bumptech.glide.Glide;
+//import com.bumptech.glide.load.engine.DiskCacheStrategy;
+//import com.bumptech.glide.load.model.GlideUrl;
+//import com.bumptech.glide.load.model.LazyHeaders;
+//import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+//import com.bumptech.glide.request.target.Target;
+//
+//import java.util.List;
+//
+//import moe.yonjigen.pvision.PvisionData;
+//import moe.yonjigen.pvision.R;
+//import moe.yonjigen.pvision.databinding.CardMainBinding;
+//import moe.yonjigen.pvision.model.MainCardModel;
+//import moe.yonjigen.pvision.utils.ImageUtil;
+//
+//public class MainCardAdapter extends RecyclerView.Adapter<MainCardAdapter.> {
+//    private List<MainCardModel> cardModelList;
+//    private Context context;
+//    private static LazyHeaders lazyHeaders = new LazyHeaders.Builder()
+//            .setHeader("Referer", PvisionData.REFERER)
+//            .setHeader("User-Agent", PvisionData.USER_AGENT)
+//            .build();
+//
+//    /**
+//     * @param context       上下文
+//     * @param cardModelList 数据源
+//     */
+//    MainCardAdapter(Context context, List<MainCardModel> cardModelList) {
+//        this.cardModelList = cardModelList;
+//        this.context = context;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//    }
+//
+//
+//    @Override
+//    public int getItemCount() {
+//        return cardModelList.size();
+//    }
+//
+//    public static void loadImage(ImageView imageView, String url) {
+//        GlideUrl glideUrl = new GlideUrl(url, lazyHeaders);
+//        Glide.with(imageView.getContext())
+//                .load(glideUrl)
+//                .transition(DrawableTransitionOptions.withCrossFade(100))
+//                .override(Target.SIZE_ORIGINAL)
+//                .override(ImageUtil.getImageWidth(imageView), ImageUtil.getImageHeight(imageView))
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                .into(imageView);
+//
+//    }
+//}
